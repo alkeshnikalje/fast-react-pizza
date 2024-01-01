5e04b59f6d2370824d077a876913c2f8d4743374
Alkesh Nikalje <alkeshnikalje@gmail.com>
2024-01-01 17:26:21 +0530

    applayout and other routes set up

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <div>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Alkesh</p>
+    </div>
+  );
+}
+
+export default Header;
