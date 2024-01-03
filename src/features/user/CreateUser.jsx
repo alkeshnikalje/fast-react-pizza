1f3a716c77ae0839510067bc387d69afbddb9e50
Alkesh Nikalje <alkeshnikalje@gmail.com>
2024-01-03 18:11:34 +0530

    create user functionality

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,29 +1,51 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateName } from "./userSlice";
+import Button from "../../ui/Button";
+import { useNavigate } from "react-router-dom";
 
 function CreateUser() {
-  const [username, setUsername] = useState('');
-
+  const [username, setUsername] = useState("");
+  const userName = useSelector((store) => store.user.userName);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   function handleSubmit(e) {
     e.preventDefault();
+    if (!username) return;
+    dispatch(updateName(username));
+    setUsername("");
+    navigate("/menu");
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <p className='mb-4 text-sm text-stone-600 md:text-base'>👋 Welcome! Please start by telling us your name:</p>
+      {userName === "" && (
+        <>
+          <p className="mb-4 text-sm text-stone-600 md:text-base">
+            👋 Welcome! Please start by telling us your name:
+          </p>
+          <input
+            type="text"
+            placeholder="Your full name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="input mb-8 w-72"
+          />
+        </>
+      )}
 
-      <input
-        type="text"
-        placeholder="Your full name"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        className='input mb-8 w-72'
-
-      />
-
-      {username !== '' && (
+      {username !== "" && (
         <div>
-          <button>Start ordering</button>
+          <Button to="/menu" type="primary">
+            START ORDERING
+          </Button>
         </div>
+      )}
+
+      {userName && (
+        <Button to="/menu" type="primary">
+          CONTINUE ORDERING, {userName}
+        </Button>
       )}
     </form>
   );
--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-    return <div className="hidden text-sm font-semibold md:block">Alkesh</div>;
-  }
-  
-  export default Username;
+  const userName = useSelector((store) => store.user.userName);
+  console.log(userName);
+  return (
+    <div className="hidden text-sm font-semibold md:block">{userName}</div>
+  );
+}
+
+export default Username;
